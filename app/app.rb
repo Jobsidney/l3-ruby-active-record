5d6213d35bf946c2f5d920302ce62942f360d841
Jobsidney <jobsidney67@gmail.com>
2022-08-25 13:04:44 +0300

    Complete

--- app/app.rb
+++ app/app.rb
@@ -2,11 +2,26 @@ require "pry"
 require "active_record"
 
 # TODO: CONNECT TO THE DATABASE
-
+ActiveRecord::Base.establish_connection(
+    adaptor:"sqlite3",
+    database:"db/school.db"
+)
 # TODO: CREATE TABLE
+create_courses_table=<<-SQL
+    CREATE TABLE IF NOT EXISTS courses(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        description TEXT,
+        duration INTEGER
+    )
+SQL
+ActiveRecord::Base.connection.execute(create_courses_table)
+
 
 # TODO: Create class that associates with the db table
+class Course< ActiveRecord::Base
 
+end
 # TODO: VIEW METHODS AVAILABLE FOR TABLE (methods)
 
 # TODO: COLUMN NAMES (column_names)
